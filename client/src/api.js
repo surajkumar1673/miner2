11c85d7e331939a9b8982ccf53bd0b08f410a797
SURAJ KUMAR <148583369+surajkumar1673@users.noreply.github.com>
2026-02-24 09:31:26 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://miner2-backend.onrender.com',
+    baseURL: 'https://miner2-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
